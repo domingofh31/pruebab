f64eacaf5b0346f187b3a794b12f008f8e022cba
Domingo Fraga Hernandez <desarrolladorrapido@gmail.com>
2025-01-31 13:20:39 -0600

    Create app.py

---
+++ app.py
@@ -0,0 +1,109 @@
+from flask import Flask
+
+from flask import render_template
+from flask import request
+from flask import jsonify, make_response
+
+import mysql.connector
+
+con = mysql.connector.connect(
+    host="185.232.14.52",
+    database="u760464709_16005339_bd",
+    user="u760464709_16005339_usr",
+    password="SUZD>3a^"
+)
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    con.close()
+
+    return render_template("app.html")
+
+@app.route("/buscar")
+def buscar():
+    if not con.is_connected():
+        con.reconnect()
+
+    cursor = con.cursor(dictionary=True)
+    cursor.execute("""
+    SELECT * FROM productos
+    """)
+    registros = cursor.fetchall()
+
+    con.close()
+
+    return make_response(jsonify(registros))
+
+@app.route("/guardar", methods=["POST"])
+def guardar():
+    if not con.is_connected():
+        con.reconnect()
+
+    id          = request.form["id"]
+    temperatura = request.form["temperatura"]
+    humedad     = request.form["humedad"]
+    
+    cursor = con.cursor()
+
+    if id:
+        sql = """
+        UPDATE sensor_log SET
+        Temperatura = %s,
+        Humedad     = %s
+        WHERE Id_Log = %s
+        """
+        val = (temperatura, humedad, id)
+    else:
+        sql = """
+        INSERT INTO sensor_log (Temperatura, Humedad, Fecha_Hora)
+                        VALUES (%s,          %s,      %s)
+        """
+        val =                  (temperatura, humedad, fechahora)
+    
+    cursor.execute(sql, val)
+    con.commit()
+    con.close()
+
+    return make_response(jsonify({}))
+
+@app.route("/editar", methods=["GET"])
+def editar():
+    if not con.is_connected():
+        con.reconnect()
+
+    id = request.args["id"]
+
+    cursor = con.cursor(dictionary=True)
+    sql    = """
+    SELECT * FROM productos
+    WHERE Id_Producto = %s
+    """
+    val    = (id,)
+
+    cursor.execute(sql, val)
+    registros = cursor.fetchall()
+    con.close()
+
+    return make_response(jsonify(registros))
+
+@app.route("/eliminar", methods=["POST"])
+def eliminar():
+    if not con.is_connected():
+        con.reconnect()
+
+    id = request.form["id"]
+
+    cursor = con.cursor(dictionary=True)
+    sql    = """
+    DELETE FROM productos
+    WHERE Id_Producto = %s
+    """
+    val    = (id,)
+
+    cursor.execute(sql, val)
+    con.commit()
+    con.close()
+
+    return make_response(jsonify({}))
